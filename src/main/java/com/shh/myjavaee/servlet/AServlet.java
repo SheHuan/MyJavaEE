fa78e1db863832813986549eed394427206af19d
shehuan <shehuan@eshangm.com>
2019-07-19 17:12:43 +0800

    test

---
+++ src/main/java/com/shh/myjavaee/servlet/AServlet.java
@@ -0,0 +1,17 @@
+package com.shh.myjavaee.servlet;
+
+
+import org.apache.catalina.mapper.Mapper;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
